c7d82fb3b924a11e728b935c3bab51c2e2071c3a
TomaszSygut <thomassygut@gmail.com>
2020-03-22 11:31:55 +0100

    stopwatch

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+import SwitchButton from './SwitchButton';
+
+class App extends Component {
+  state = {
+    time: 0,
+    active: false
+  }
+  handleClick = () => {
+    if (this.state.active) {
+      clearInterval(this.idInterval)
+
+    }
+    else {
+      this.idInterval = setInterval(() => this.addSecond(), 1000)
+
+    }
+
+    this.setState({
+      active: !this.state.active
+    })
+
+
+
+  }
+  addSecond = () => {
+    this.setState({
+      time: this.state.time + 1
+    })
+  }
+  render() {
+
+    return (
+      <Fragment>
+        <p>{this.state.time}</p>
+        <SwitchButton click={this.handleClick} active={this.state.active} />
+
+      </Fragment>
+    )
+  }
+}
+
+export default App;
